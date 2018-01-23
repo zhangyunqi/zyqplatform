eeec0ffa6c7dbd6a26e3447c5823bbfe50eb173d
张运奇 <zhangyq@gitlab.com>
2018-01-23 17:35:45 +0800

    将默认的Logback改成log4j

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	static{
+		Logger logger = Logger.getLogger(DemoApplication.class);
+		logger.info("111111");
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
